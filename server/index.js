175fdb95d8046b719eb058cead6cf15655635bc4
Espadv69 <aitorjob69@gmail.com>
2025-02-11 18:01:13 +0100

    Add User model

--- server/index.js
+++ server/index.js
@@ -1,12 +1,24 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 
 dotenv.config()
 
 const app = express()
 app.use(express.json())
 
-mongoose.connect(process.env.MONGO_URI)
+// Conectar a MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected ✅'))
   .catch((err) => console.error('MongoDB connection failed ❌', err))
+
+// Modelo de usuario
+const User = mongoose.model(
+  'User',
+  new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+  })
+)
