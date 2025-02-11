26ef0f128e6578d39294991379593203bdc3c7fa
Espadv69 <aitorjob69@gmail.com>
2025-02-11 17:59:21 +0100

    Add mongoose connect

--- server/index.js
+++ server/index.js
@@ -1,3 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected ✅'))
+  .catch((err) => console.error('MongoDB connection failed ❌', err))
